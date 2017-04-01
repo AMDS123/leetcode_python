57d3668af7cc7a20fc8fd5c1dba2931dff3cbb26
450076077@qq.com <450076077@qq.com>
2017-04-01 10:57:51 +0800

    Two Sum

--- main.py
+++
@@ -1,5 +0,0 @@
-import twosum
-
-s = twosum.Solution()
-result = s.twoSum([2,7,11,15], 9)
-print(result)
--- twosum.py
+++ twosum.py
@@ -5,5 +5,16 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
+        map = {}
+        for i in xrange(len(nums)):
+            map[nums[i]] = i
+        for i in xrange(len(nums)):
+            num = nums[i]
+            num1 = target - num
+            if map.has_key(num1):
+                j = map[num1]
+                if i != j:
+                    return [i,j]
 
-        return [0]
+result = Solution().twoSum([2,7,11,15], 9)
+print(result)
