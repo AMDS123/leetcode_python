0a697ca48373c6e2d5c0fe4f08f940902b0b4ed0
450076077@qq.com <450076077@qq.com>
2017-04-05 15:51:00 +0800

    add

---
+++ PalindromeNumber.py
@@ -0,0 +1,16 @@
+# Determine whether an integer is a palindrome.
+# Do this without extra space.
+
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0 or x != 0 and x % 10 == 0:
+            return False
+        rev = 0
+        while x > rev:
+            rev = rev * 10 + x % 10
+            x /= 10
+        return x == rev or x == rev / 10
